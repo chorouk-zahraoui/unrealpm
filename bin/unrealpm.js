bb6f877a42530529512cb980d2eb4c151f623921
enrico-giovanni <enrico-giovanni@trampolier.com>
2017-08-17 21:50:00 +0200

    upm init, upm install and upm publish

--- bin/unrealpm.js
+++ bin/unrealpm.js
@@ -1,30 +1,55 @@
 #!/usr/bin/env node
 
 const
-    program = require('commander'),
-    log = console.log;
+  program = require('commander'),
+  npm = require('npm'),
+  fs = require('fs'),
+  log = console.log;
 
 program
-    .version('1.0.3');
+  .version('1.0.4');
 
 program
-    .command('init')
-    .description('generates the package.json file')
-    .action(() => {
-        log('init');
-});
+  .command('init')
+  .description('Generate the package.json file')
+  .action(() => {
+    npm.load({}, () => {
+      npm.init(err => {
+        if (err) {
+          log(err);
+        } else {
+          const pkg = JSON.parse(fs.readFileSync('package.json'));
+          pkg.unreal_project = true;
+          fs.writeFileSync('package.json', JSON.stringify(pkg, undefined, 2));
+        }
+      });
+    })
+  });
 
 program
-    .command('install <module>')
-    .description('install the specified cpp module or dependency')
-    .option(' --save', 'save to package.json file' )
-    .action((module, options) => {
-        log(module);
-     });
+  .command('install <module>')
+  .description('Install the specified Unreal module')
+  .option(' --save', 'save to package.json file')
+  .action((mdl, options) => {
+    const config = {};
+    options.save && (config.save = true);
+    npm.load(config, () => {
+      npm.install(mdl, err => {
+        log(err, 'done');
+      });
+    })
+  });
 
 program
-    .command('publish')
-    .description('publish on npm registry your code');
+  .command('publish')
+  .description('Publish the Unreal module on the npm registry')
+  .action(() => {
+    npm.load({}, () => {
+      npm.publish((err) => {
+        err && log(err);
+      })
+    })
+  });
 
 program
-    .parse(process.argv);
+  .parse(process.argv);
